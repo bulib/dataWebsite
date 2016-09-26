05b2fb99ef2c0db60ff6a88bd29ff5245b58496e
tomhohenstein <sirtah@gmail.com>
2016-09-26 11:57:44 -0400

    automatic jekyll rebuilds

--- _site/assets/js/custom.js
+++ _site/assets/js/custom.js
@@ -11,5 +11,28 @@ $(function() {
   		}
 	});
 
+	//offset side nav on pages 
 
+	var offsetheight = 75; 
+	//adjusts for the navbar height while scrolling
+	$('body').scrollspy({
+   		offset:  offsetheight 
+	});
+
+	$('#toc').affix({
+		offset: {
+			top: 0,
+    		bottom: function () {
+      			return (this.bottom = $('.footer').outerHeight(true))
+    		}
+    	}	
+	})
+
+	//adjusts for click on sidebar
+	$("#toc .nav li a").click(function(event) {
+	    event.preventDefault();
+	    console.log("click");
+	    $($(this).attr('href'))[0].scrollIntoView();
+	    scrollBy(0, -offsetheight);
+	});
 })
